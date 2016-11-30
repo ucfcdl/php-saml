5abe95dd9c59fd28764591722317c95db44b3b2e
iturgeon <iturgeon@gmail.com>
2016-11-30 10:08:14 -0500

    fix merge issue

--- lib/Saml2/Response.php
+++ lib/Saml2/Response.php
@@ -569,7 +569,6 @@ class OneLogin_Saml2_Response
 
             $attributeValues = array();
             foreach ($entry->childNodes as $childNode) {
-                if ($childNode->nodeType == XML_ELEMENT_NODE && ($childNode->tagName === 'saml:AttributeValue' || $childNode->tagName === 'saml2:AttributeValue')) {
                 $tagName = ($childNode->prefix ? $childNode->prefix.':' : '') . 'AttributeValue';
                 if ($childNode->nodeType == XML_ELEMENT_NODE && $childNode->tagName === $tagName) {
                     $attributeValues[] = $childNode->nodeValue;
